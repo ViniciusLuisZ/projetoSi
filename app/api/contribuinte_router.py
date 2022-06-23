44251b6838a176071b2f138ce1606c38fc1c4e57
unknown <vlczorzella@hotmail.com>
2022-06-23 18:18:29 -0400

    moveEnums

--- app/api/contribuinte_router.py
+++ app/api/contribuinte_router.py
@@ -1,5 +1,7 @@
 from app.api import api_router
 from fastapi import APIRouter, Depends
+from app import enums
+
 # from app.domain.models.contribuente import ContribuinteQuerry
 
 router = APIRouter()
@@ -9,6 +11,18 @@ router = APIRouter()
 def hello_world_root():
     return {"Hello": "World"}
 
+
+@router.get('/situpj/{lang}')
+def getenum (lang:enums.indSitPJ):
+    if (lang == enums.indSitPJ.extinto):
+        return {"Pessoa Extinta"}
+    elif (lang == enums.indSitPJ.fusao):    
+        return {"Pessoa com Fusão"}
+    elif (lang == enums.indSitPJ.situaNormal):
+        return {"Pessoa Em Situação Normal"}
+    elif (lang == enums.indSitPJ.cisao):
+        return {"Pessao Em Cisao"}
+    return {"Pessoa Incorporada"}
 
 # @router.post('/', response_model="teste")
 # def contribuente(param: ContribuinteQuerry = Depends()):
--- app/main.py
+++ app/main.py
@@ -3,7 +3,6 @@ from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 from app.api.api_router import router
 from app.core.config import settings
-from app import enums
 
 # Base.metadata.create_all(bind=engine)
 
@@ -33,15 +32,3 @@ app = get_application()
 if __name__ == '__main__':
     uvicorn.run(app, host="0.0.0.0", port=8000)
 
-
-@app.get('/situpj/{lang}')
-def getenum (lang:enums.indSitPJ):
-    if (lang == enums.indSitPJ.extinto):
-        return {"Pessoa Extinta"}
-    elif (lang == enums.indSitPJ.fusao):    
-        return {"Pessoa com Fusão"}
-    elif (lang == enums.indSitPJ.situaNormal):
-        return {"Pessoa Em Situação Normal"}
-    elif (lang == enums.indSitPJ.cisao):
-        return {"Pessao Em Cisao"}
-    return {"Pessoa Incorporada"}
