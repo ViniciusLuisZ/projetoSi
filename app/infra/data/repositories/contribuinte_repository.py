59800f06b69bb0c17d6876455e70a80ab0e85d10
guih50 <guih50@hotmail.com>
2022-06-24 18:37:29 -0400

    feat: Add filter to select inclusion only

--- app/infra/data/repositories/contribuinte_repository.py
+++ app/infra/data/repositories/contribuinte_repository.py
@@ -3,7 +3,7 @@ from app.infra.data.db import database_r1000
 
 async def get_contribuintes():
     data = []
-    for doc in database_r1000.Reinf.find():
+    for doc in database_r1000.Reinf.find({'evtInfoContri.infoContri.inclusao': {'$ne': None}}):
         doc['_id'] = str(doc['_id'])  # Convertendo _id para iterar cursor
         data.append(doc)
     return data
