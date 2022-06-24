2918148ca070e2ddd92e3e86b877c0aae8a01c3c
guih50 <guih50@hotmail.com>
2022-06-24 18:53:27 -0400

    fix: remove _id from response

--- app/infra/data/repositories/contribuinte_repository.py
+++ app/infra/data/repositories/contribuinte_repository.py
@@ -2,11 +2,7 @@ from app.infra.data.db import database_r1000
 
 
 async def get_contribuintes():
-    data = []
-    for doc in database_r1000.Reinf.find({'evtInfoContri.infoContri.inclusao': {'$ne': None}}):
-        doc['_id'] = str(doc['_id'])  # Convertendo _id para iterar cursor
-        data.append(doc)
-    return data
+    return [x['evtInfoContri'] for x in database_r1000.Reinf.find({'evtInfoContri.infoContri.inclusao': {'$ne': None}})]
 
 async def get_contribuentes_details(id):
     return [x['evtInfoContri'] for x in database_r1000.Reinf.find({'evtInfoContri.id': {'$eq': id}})]
