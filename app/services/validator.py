f1c55946d3bd97cf60489efb1b366aceee56b80a
thevitorferreira <vitorlucasbarboza@hotmail.com>
2022-06-24 20:49:06 -0400

    feat: adiciona regra de validação clasTrib

--- app/services/validator.py
+++ app/services/validator.py
@@ -1,1 +1,10 @@
 ### esse script será usado para adicionar as regras de validação dos campos do reinf
+import app.exceptions as app_exceptions
+
+def validate_classTrib(data):
+    if data['ideContri']['tpInsc'] == '2':
+        if data['infoContri']['inclusao']['infoCadastro']['clasTrib'] not in ['21', '22']:
+            raise app_exceptions.Invalidinput("Valor não aceito")
+    if data['ideContri']['tpInsc'] == '1':
+        if data['infoContri']['inclusao']['infoCadastro']['clasTrib'] in ['21', '22']:
+            raise app_exceptions.Invalidinput("Valor não aceito")
