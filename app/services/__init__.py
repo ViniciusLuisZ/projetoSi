185663cbf11d8aa0bbb2c19412bc58d944e50e02
Eduardo <eazevedo.016@gmail.com>
2022-07-02 19:48:21 -0400

    add metodos restantes no cabeçalho da service

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -4,3 +4,6 @@ def contribuente_validator(infos):
     validate_classTrib(infos)
     validate_email(infos)
     validate_cpf(infos)
+    foneFixoOuCel(infos)
+    validate_foneCel(infos)
+    validate_foneFixo(infos)
